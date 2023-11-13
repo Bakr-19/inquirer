cf857716da39ad3ae93c61b3b15ed6651eb8e810
Bakr-19 <141832833+Bakr-19@users.noreply.github.com>
2023-11-13 09:36:57 +0000

    New commitment

--- copyies.js
+++ copyies.js
@@ -1,58 +1,57 @@
-// import inquirer from 'inquirer';
+import inquirer from 'inquirer';
 
 
-// const questions = [
-// /* Pass your questions in here */
-// {
-//   type: 'input',
-//   name: 'name',
-//   message: 'What is your name?',
-//   validate: (answer) => {
-//     if(answer == ''){
-//       return 'Please enter a vaild name'
-//     }
-//       return true
+const questions = [
+/* Pass your questions in here */
+{
+  type: 'input',
+  name: 'name',
+  message: 'What is your name?',
+  validate: (answer) => {
+    if(answer == ''){
+      return 'Please enter a vaild name'
+    }
+      return true
 
-//   }
-// },
-// {
-//   type: 'input',
-//   name: 'height',
-//   message: 'What is your height (in inches)?',
-//   validate: (answer) => {
-//     if(isNaN(answer)){
-//       return 'Please enter a vaild Number'
-//     }
-//       return true
+  }
+},
+{
+  type: 'input',
+  name: 'height',
+  message: 'What is your height (in inches)?',
+  validate: (answer) => {
+    if(isNaN(answer)){
+      return 'Please enter a vaild Number'
+    }
+      return true
 
-//   }
-// },
-// {
-//   type: 'input',
-//   name: 'weight',
-//   message: 'What is your weight (in pounds)?',
-//   validate: (answer) => {
-//     if(isNaN(answer)){
-//       return 'Please enter a vaild Number'
-//     }
-//       return true
+  }
+},
+{
+  type: 'input',
+  name: 'weight',
+  message: 'What is your weight (in pounds)?',
+  validate: (answer) => {
+    if(isNaN(answer)){
+      return 'Please enter a vaild Number'
+    }
+      return true
 
-//   }
-// },
+  }
+},
 
-// ]
-// console.log('----------------BMI Cal----------------')
-// inquirer
-//   .prompt(questions)
-//   .then((answers) => {
-//     const height = answers.height
-//     const weight = answers.weight
-//     const bmi = (weight/(height*height))*703
-//     if (bmi < 16){
-//       console.log(`${answers.name} your BMI is ${bmi} and it is consiered to be severly under weight`)
-//     }else if(bmi >= 18.5 ){
-//       console.log(`${answers.name} your BMI is ${bmi} and it is consiered to be severly under normal`)
-//     }
-//   })
+]
+console.log('----------------BMI Cal----------------')
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    const height = answers.height
+    const weight = answers.weight
+    const bmi = (weight/(height*height))*703
+    if (bmi < 16){
+      console.log(`${answers.name} your BMI is ${bmi} and it is consiered to be severly under weight`)
+    }else if(bmi >= 18.5 ){
+      console.log(`${answers.name} your BMI is ${bmi} and it is consiered to be severly under normal`)
+    }
+  })
 
-console.log(Math.floor(Math.random()* 3))
